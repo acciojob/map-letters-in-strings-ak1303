e865283394e4f67a843a15870b7846a167468c49
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 00:42:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let obj ={};
+	for(let i=0;i<str.length;i++){
+		if(obj[str[i]]==undefined)obj[str[i]]=[];
+		obj[str[i]].push(i);
+	}
+	return obj;
+}
+const l = prompt("Enter a String.");
+alert(mapLetters(l));
